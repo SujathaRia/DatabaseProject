79832c9e556717f9a45c3a072a840a31f752dbb7
SujathaRia <you@example.com>
2023-04-26 09:41:01 +0000

    Saving Database project

---
+++ src/test/DeleteExample.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteExample {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "root");
+
+		Statement stmt = con.createStatement();
+
+		stmt.executeUpdate(" delete from eproduct where name = 'Television';");
+
+		System.out.println("The row is inserted");
+
+		ResultSet result = stmt.executeQuery("select * from eproduct");
+
+		while (result.next()) {
+
+			System.out.println("Product ID " + result.getInt("ID"));
+			System.out.println("Product name " + result.getString("name"));
+			System.out.println("Price is " + result.getDouble("Price"));
+
+		}
+
+		con.close();
+
+
+	}
+
+}
